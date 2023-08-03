824505820735ba982754c8b4637e75a5133f5bed
daanh432 <8281824+daanh432@users.noreply.github.com>
2023-08-03 22:43:16 +0200

    Updated handling of template config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 # Replace WEB_ROOT in nginx.conf template with the value from the WEB_ROOT environment variable
 if [ -n "$WEB_ROOT" ]; then
-  sed "s|root /var/www/html;|root ${WEB_ROOT};|" /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
+  sed "s|root /var/www/html;|root ${WEB_ROOT};|" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
   echo "WEB_ROOT environment variable set. Using the WEB_ROOT value: ${WEB_ROOT}"
 else
+  cp /etc/nginx/conf.d/default.conf.template /etc/nginx/conf.d/default.conf
   echo "WEB_ROOT environment variable not set. Using the default WEB_ROOT value: /var/www/html"
 fi
 
