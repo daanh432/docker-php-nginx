78b02b763be9bf8a798fa067cdd62d07f080812e
daanh432 <8281824+daanh432@users.noreply.github.com>
2023-08-03 22:23:33 +0200

    Fix warning messagees

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Replace WEB_ROOT in nginx.conf template with the value from the WEB_ROOT_ENV environment variable
+# Replace WEB_ROOT in nginx.conf template with the value from the WEB_ROOT environment variable
 if [ -n "$WEB_ROOT" ]; then
   sed "s|root /var/www/html;|root ${WEB_ROOT};|" /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 else
-  echo "WEB_ROOT_ENV environment variable not set. Using the default WEB_ROOT value."
+  echo "WEB_ROOT environment variable not set. Using the default WEB_ROOT value."
 fi
 
 # Start supervisord
