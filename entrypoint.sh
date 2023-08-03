adf91f84e5142c8c734b5013ab57d23e2828dd68
daanh432 <8281824+daanh432@users.noreply.github.com>
2023-08-03 22:24:33 +0200

    Added more information

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 # Replace WEB_ROOT in nginx.conf template with the value from the WEB_ROOT environment variable
 if [ -n "$WEB_ROOT" ]; then
   sed "s|root /var/www/html;|root ${WEB_ROOT};|" /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
+  echo "WEB_ROOT environment variable set. Using the WEB_ROOT value: ${WEB_ROOT}"
 else
-  echo "WEB_ROOT environment variable not set. Using the default WEB_ROOT value."
+  echo "WEB_ROOT environment variable not set. Using the default WEB_ROOT value: /var/www/html"
 fi
 
 # Start supervisord
